db07d59b091be9af1bb6471e91be560ffeda1be7
Igogrek <igogrek@gmail.com>
2018-07-19 17:17:59 +0300

    Added i18n

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './styles.scss';
+import {i18n} from './i18n';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app');
